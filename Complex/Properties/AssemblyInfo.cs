d4e59004fffb610e9c372e61d6c73d5fdcf9f3ad
Dmitriy Volkov <mcslash@yandex.ru>
2019-01-28 12:52:46 +0300

    ДЗ3 без звёздочек.

--- Complex/Properties/AssemblyInfo.cs
+++ Complex/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AAA")]
+[assembly: AssemblyTitle("Complex")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AAA")]
+[assembly: AssemblyProduct("Complex")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9616c2ce-8d72-437a-b461-13bef5fa452c")]
+[assembly: Guid("a011f6e3-e10c-4739-8812-41e6cdf4387b")]
 
 // Version information for an assembly consists of the following four values:
 //
